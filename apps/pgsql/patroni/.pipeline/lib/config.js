fcee0c7b64da3a4ff69543c77f41403c950c34a5
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-02-18 22:28:49 -0800

    Update config.js

--- apps/pgsql/patroni/.pipeline/lib/config.js
+++ apps/pgsql/patroni/.pipeline/lib/config.js
@@ -1,13 +1,13 @@
 'use strict';
 const options= require('pipeline-cli').Util.parseArguments()
 const changeId = options.pr //aka pull-request
-const version = '10.0'
+const version = '10'
 const name = 'patroni'
 
 const phases = {
-  build: {namespace:'bcgov-tools' , name: `${name}`, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${name}-build-${changeId}`, version:`${version}-${changeId}`, tag:`v${version}-${changeId}`},
-   test: {namespace:`bcgov`,        name: `${name}`, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${name}-test`             , version:`${version}-${changeId}`, tag:`v${version}-latest`},
-   prod: {namespace:`bcgov`,        name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , version:`${version}-${changeId}`, tag:`v${version}-stable`}
+  build: {namespace:'bcgov-tools' , name: `${name}`, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${name}-build-${changeId}`, tag:`v${version}-${changeId}`},
+   test: {namespace:`bcgov`,        name: `${name}`, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${name}-test`             , tag:`v${version}-latest`},
+   prod: {namespace:`bcgov`,        name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , tag:`v${version}-stable`}
 }
 
 module.exports = exports = phases
