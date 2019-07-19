41ef21fc53a36aedae8e5b66a3331cd714965143
Dave Kelsey <David.Kelsey@gov.bc.ca>
2019-07-19 10:30:33 -0700

    add minishift restart script.

---
+++ wiki/assets/restart-minishift.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+:<<'END'
+
+Author: Dave Kelsey <David.Kelsey@gov.bc.ca>
+  Date: July 15, 2019
+
+Restarting minishift is tedious so I scripted it.
+
+
+PRECONDITIONS:  
+
+* You are not using windows - I don't have a widows machine to test this on.
+* You have already setup minishift and you are simply restarting it.
+
+minishift Installation Instructions:
+
+https://github.com/BCDevOps/platform-services/blob/cvarjao-cdk-minishift/wiki/minishift.md
+
+
+I want to stress this is a restart script!
+
+
+NOTES:
+
+* This script will not re-setup Persisten Volumes: I don't believe that to be necessary provided
+  you already did so when you initially setup minishift. As the `admin` user run `oc get pv` and you see this is correct.
+* Associating a develoepr account so that images can be pulled from the redhat registry assumes you have a developer account and
+  that your subscription Pool ID (the value supplied to '--pool')  doesn't change.  Developer accounts are free (https://developer.redhat.com).
+  From the following instructions you can find your Pool ID: 
+
+  https://github.com/BCDevOps/platform-services/blob/cvarjao-cdk-minishift/wiki/minishift.md#attach-rhel-subscription-enables-red-hat-software-collections
+
+
+END
+
+echo "stopping minishift"
+minishift stop
+
+echo "starting minishift"
+minishift start --openshift-version=3.11.59 --memory=12GB --cpus=8
+
+echo "Setting hostname for docker registry"
+minishift ssh -- 'sudo bash -c "echo 172.30.1.1 docker-registry.default.svc >> /etc/hosts"'
+
+echo "whoami?"
+oc whoami
+
+echo "authenticate as admin"
+oc login -u admin -p system
+
+echo "set REGISTRY_OPENSHIFT_SERVER_ADDR for 'default' project/namespacer"
+oc -n default set env dc/docker-registry REGISTRY_OPENSHIFT_SERVER_ADDR=docker-registry.default.svc:5000
+
+echo "authenticate as developer again"
+oc login -u developer -p hello
+
+echo "Associate deverloper account - so that image repose can be found"
+minishift ssh -- 'sudo bash -c "subscription-manager attach --pool=8a85f9916977b22c0169aafb6379038b"'
+
+echo "confirm that the RHSCL imager repos can be found"
+minishift ssh -- 'sudo bash -c "subscription-manager repos --list | grep scl"'
