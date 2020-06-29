d3cd87ef22cf41e69ab6d4cb4c06ad8a3328fadf
ShellyXueHan <shelly.han@gov.bc.ca>
2020-06-29 16:52:54 -0700

    rename cpu report output

--- tools/audit/repo-mapper/audit-playbook/ansible/projstats.sh
+++ tools/audit/repo-mapper/audit-playbook/ansible/projstats.sh
@@ -1,6 +1,6 @@
-rm $OUTPUT_PATH/project-stats.csv;
-echo "sep=;" >> $OUTPUT_PATH/project-stats.csv
-echo "\"Namespace\";\"Display Name\";\"Product Lead\";\"Product Owner\";\"Environment\";\"Product\";\"Project Type\";\"Team\";\"CPU Usage\";\"CPU Requests\";\"CPU Limits\";\"CPU Requests %\";\"CPU Limits %\"" >> $OUTPUT_PATH/project-stats.csv
+rm $OUTPUT_PATH/project_stats.csv;
+echo "sep=;" >> $OUTPUT_PATH/project_stats.csv
+echo "\"Namespace\";\"Display Name\";\"Product Lead\";\"Product Owner\";\"Environment\";\"Product\";\"Project Type\";\"Team\";\"CPU Usage\";\"CPU Requests\";\"CPU Limits\";\"CPU Requests %\";\"CPU Limits %\"" >> $OUTPUT_PATH/project_stats.csv
 # for NS in $(oc get -o=custom-columns=NAME:.metadata.name --no-headers projects); do
 for NS in $(oc get -o=custom-columns=NAME:.metadata.name --no-headers projects -l name=devhub); do
   NS_DATA=$(oc get ns -o json "${NS}")
@@ -16,5 +16,5 @@ for NS in $(oc get -o=custom-columns=NAME:.metadata.name --no-headers projects -
   CPU_LIMITS=$(curl -sk -H "Authorization: Bearer $(oc -n devhub-tools sa get-token cluster-view-sa)" "https://prometheus-k8s-openshift-monitoring.pathfinder.gov.bc.ca/api/v1/query?query=sum(kube_pod_container_resource_limits_cpu_cores%7Bnamespace%3D%22${NS}%22%7D%20*%20ON(node)%20group_left()%20count(kube_node_labels%7Blabel_region%3D%22app%22%7D)%20by%20(node)%20*%20ON(namespace%2Cpod%2Ccontainer)%20kube_pod_container_status_running%20%3E%200)%20by%20(namespace)" | ./jq -r '.data.result[0].value[1]')
 
   # Echo data
-  echo "\"${NS}\";\"${DISPLAY_NAME}\";\"${PRODUCT_LEAD}\";\"${PRODUCT_OWNER}\";\"${NS_ENVIRONMENT}\";\"${PRODUCT}\";\"${PROJECT_TYPE}\";\"${TEAM}\";\"${CPU_USAGE}\";\"${CPU_REQUEST}\";\"${CPU_LIMITS}\"" >> $OUTPUT_PATH/project-stats.csv
+  echo "\"${NS}\";\"${DISPLAY_NAME}\";\"${PRODUCT_LEAD}\";\"${PRODUCT_OWNER}\";\"${NS_ENVIRONMENT}\";\"${PRODUCT}\";\"${PROJECT_TYPE}\";\"${TEAM}\";\"${CPU_USAGE}\";\"${CPU_REQUEST}\";\"${CPU_LIMITS}\"" >> $OUTPUT_PATH/project_stats.csv
 done
