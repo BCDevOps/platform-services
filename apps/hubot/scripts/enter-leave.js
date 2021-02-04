731d0547fe3909abea96d65ac6c462d4ee2b8cb8
Cailey Jones <cailey.jones@gov.bc.ca>
2021-02-04 15:53:15 -0800

    add enter and leave script

---
+++ apps/hubot/scripts/enter-leave.js
@@ -0,0 +1,13 @@
+
+
+  const enterReplies = ['Hi!', 'Welcome!', 'Hello friend!'];
+  const leaveReplies = ['See you later!'];
+
+  module.exports = function(robot) {
+    robot.enter(function(res) {
+
+    });
+    return robot.leave(function(res) {
+
+    });
+  };
