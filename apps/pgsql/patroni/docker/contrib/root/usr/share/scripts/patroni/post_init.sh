6f1d3129ec6051cf4345e8c1e451e772ed987c33
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2021-04-16 15:01:16 -0700

    Update post_init.sh

--- apps/pgsql/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ apps/pgsql/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -9,6 +9,10 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
   psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
   # echo "Enabling PostGIS extension"
+  echo "$1"
+  echo "${APP_USER}"
+  echo "${APP_PASSWORD}"
+  echo "${APP_DATABASE}"
   # psql "$1" -d "${APP_DATABASE}" -w -c "CREATE EXTENSION postgis"
 else
   echo "Skipping user creation"
