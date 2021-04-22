cb7f0a8b4cbb6f33049337594d725e32eb88d461
Shelly Han <shelly.han@gov.bc.ca>
2021-04-22 11:33:18 -0700

    Cerberus build - docker (#755)
    * added github workflow for cerberus docker image build
    
    * test build off cluster
    
    * ignore entitlement certs
    
    * added docs for manual docker image build
    
    * opt out cerberus image build as missing cert
    
    * switch configs to templates
    
    * added namespace role to grab service account token
    
    * update order
    
    * entrypoint scripts
    
    * mount config file template and update container command
    
    * point to docker image instead
    
    * update namespace
    
    * incluster cluster config using service name
    
    * added step to insert ca cert
    
    * rename role
    
    * Update monitoring/cerberus/Dockerfile
    
    Co-authored-by: Steven Barre <steven@stevenbarre.com>
    
    * Update monitoring/cerberus/readme.md
    
    Co-authored-by: Steven Barre <steven@stevenbarre.com>
    
    * empty line
    
    * no longer need to enable extra repo for python3-devel
    
    * added resource config
    
    Co-authored-by: Steven Barre <steven@stevenbarre.com>

---
+++ monitoring/cerberus/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This is to get the cerberus Service Account token
+export SA_TOKEN=$(oc serviceaccounts get-token cerberus)
+# and then feed into the configuration files that needs it
+sed "s/SA_TOKEN/${SA_TOKEN}/g" /tmp/kubeconfig-template > /tmp/kubeconfig-tmp
+sed "s/SA_TOKEN/${SA_TOKEN}/g" /tmp/cerberus-config-template.yaml > /tmp/cerberus-config.yaml
+
+# insert CA cert:
+export CA_CERT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt | base64 -i - | sed -z 's/\n//g' -)
+sed "s/CA_CERT/${CA_CERT}/" /tmp/kubeconfig-tmp > /tmp/kubeconfig
+
+cerberus_client -c /tmp/cerberus-config.yaml
