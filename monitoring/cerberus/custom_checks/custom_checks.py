cfc030d062ae4a8adae16065948be6e1304bb56e
Shelly Han <shelly.han@gov.bc.ca>
2021-06-10 11:11:57 -0700

    added regiry and routing monitor (#766)

--- monitoring/cerberus/custom_checks/custom_checks.py
+++ monitoring/cerberus/custom_checks/custom_checks.py
@@ -15,7 +15,7 @@ def check_nodes():
 
   up_ratio = int(node_count)/int(total_node_count)
 
-  return True if up_ratio > 0.8 else False
+  return up_ratio > 0.8
 
 def check_cluster_readyz():
   logging.info("Check cluster readyz endpoint.")
@@ -24,12 +24,19 @@ def check_cluster_readyz():
   api_server_readyz_url = cluster_api_url.split(" ")[-1].strip() + "/readyz"
   (resp, content) = h.request(api_server_readyz_url, "GET")
   
-  return True if "ok" in str(content) else False
+  return "ok" in str(content)
 
-def check_image_registry():
-  logging.info("Check Image Registry API.")
+def check_image_registry_and_routing():
+  logging.info("Check Image Registry API and test on routing layer.")
 
-  return True
+  # get image_registry URL:
+  image_registry_route = runcommand.invoke("oc -n openshift-image-registry get route/public-registry -o json")
+  image_registry_host = eval(image_registry_route)['spec']['host']
+  image_registry_url = "https://" + image_registry_host + "/healthz"
+  logging.info("Detected Image Registry API: " + image_registry_url)
+  (resp, content) = h.request(image_registry_url, "GET")
+
+  return resp.status == 200
 
 def main():
   logging.info("------------------- Start Custom Checks -------------------")
@@ -44,7 +51,7 @@ def main():
 
   check1 = check_nodes()
   check2 = check_cluster_readyz()
-  check3 = check_image_registry()
+  check3 = check_image_registry_and_routing()
   logging.info("------------------- Finished Custom Checks -------------------")
 
   return check1 & check2 & check3
