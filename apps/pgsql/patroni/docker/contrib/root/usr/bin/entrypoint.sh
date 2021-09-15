037a157ff04bcee91accb1c2f1fb55d3213e7adf
Cailey Jones <cailey.jones@gov.bc.ca>
2021-09-15 14:49:47 -0700

    add configmap

--- apps/pgsql/patroni/docker/contrib/root/usr/bin/entrypoint.sh
+++ apps/pgsql/patroni/docker/contrib/root/usr/bin/entrypoint.sh
@@ -12,33 +12,7 @@ mkdir -p "$PATRONI_POSTGRESQL_DATA_DIR"
 chmod 700 "$PATRONI_POSTGRESQL_DATA_DIR"
 
 cat > /home/postgres/patroni.yml <<__EOF__
-bootstrap:
-  post_bootstrap: /usr/share/scripts/patroni/post_init.sh
-  dcs:
-    postgresql:
-      use_pg_rewind: true
-      parameters:
-        max_connections: ${POSTGRESQL_MAX_CONNECTIONS:-100}
-        max_prepared_transactions: ${POSTGRESQL_MAX_PREPARED_TRANSACTIONS:-0}
-        max_locks_per_transaction: ${POSTGRESQL_MAX_LOCKS_PER_TRANSACTION:-64}
-  initdb:
-  - auth-host: md5
-  - auth-local: trust
-  - encoding: UTF8
-  - locale: en_US.UTF-8
-  - data-checksums
-  pg_hba:
-  - host all all 0.0.0.0/0 md5
-  - host replication ${PATRONI_REPLICATION_USERNAME} ${POD_IP}/16    md5
-restapi:
-  connect_address: '${POD_IP}:8008'
-postgresql:
-  connect_address: '${POD_IP}:5432'
-  authentication:
-    superuser:
-      password: '${PATRONI_SUPERUSER_PASSWORD}'
-    replication:
-      password: '${PATRONI_REPLICATION_PASSWORD}'
+${PATRONI_CONFIG}
 __EOF__
 
 unset PATRONI_SUPERUSER_PASSWORD PATRONI_REPLICATION_PASSWORD
